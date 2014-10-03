7d9e41c4c3cab497a419c78a75e7598c57dcbe54
Erin Hinkle <erinhinx5@gmail.com>
2014-10-03 13:10:28 -0400

    input

--- js/app.js
+++ js/app.js
@@ -40,6 +40,39 @@ $(document).ready(function(){
   	/*--- Collect, Store, Count Guesses ---*/
   	var storedGuesses = [];
 
+  	$('#guessButton').click(function() {
+  		takeGuess();
+  	});
+  	$('input#userGuess'.on('keypress', function(event) {
+  		if( event.which == 13) {
+  			event.preventDefault();
+  			takeGuess();
+  		}
+  	});
+
+  	//function for making sure user enters a number
+  	var numCheck = function() {
+  		var userGuess = +$('#userGuess').val();
+  		if(isNaN(userGuess)) {
+		alert('Please Choose A Number')	
+		} else if (userGuess %1 !=0) {
+		alert('Please Choose A Whole Integer')
+		} else {
+		takeGuess();
+		}
+	}; 
+
+
+  	var takeGuess = function(userGuess) {
+  		var userGuess = +$('#userGuess').val();
+  		
+
+
+  	}
+
+
+
+
   		/*--- Display Guesses ---*/
 
 
